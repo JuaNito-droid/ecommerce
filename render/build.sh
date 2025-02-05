bf32451da6d26d5f4a9891165bcc47a655430072
Andyy <andymaytac@gmail.com>
2025-02-05 00:56:42 -0400

    update_superUser

--- render/build.sh
+++ render/build.sh
@@ -10,3 +10,13 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+
+# Crear un superusuario no interactivo con Django en el entorno de producción
+python manage.py shell <<EOF
+from django.contrib.auth.models import User
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "adminpassword")
+EOF
+
+echo "Superusuario creado exitosamente (si no existía previamente)."
