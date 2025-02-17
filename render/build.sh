6c0dab8b7f6c7f70090b3b293ceaf39aed0e3a8d
Andyy <andymaytac@gmail.com>
2025-02-17 00:30:42 -0400

    ultima

--- render/build.sh
+++ render/build.sh
@@ -18,3 +18,20 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Crear un superusuario si no existe (esto es para evitar errores si el superusuario ya está creado)
+python manage.py shell <<EOF
+from apps.user.models import UserAccount
+from django.db.utils import IntegrityError
+
+try:
+    # Verificar si el superusuario ya existe
+    if not UserAccount.objects.filter(email="$EMAIL").exists():
+        # Crear superusuario
+        UserAccount.objects.create_superuser(email="$EMAIL", password="$PASSWORD", first_name="Admin", last_name="User")
+        print("Superuser created successfully.")
+    else:
+        print("Superuser already exists.")
+except IntegrityError as e:
+    print(f"Error creating superuser: {e}")
+EOF
