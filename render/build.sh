74025675f3d349dfd74d86e4374b24350f67a248
Andyy <andymaytac@gmail.com>
2025-02-17 01:14:48 -0400

    ultima4

--- render/build.sh
+++ render/build.sh
@@ -9,7 +9,6 @@ export EMAIL="admin@vudera.com"
 export PASSWORD="admin_password"
 
 
-pip install setuptools wheel
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
@@ -19,20 +18,3 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
-
-# Crear un superusuario si no existe (esto es para evitar errores si el superusuario ya está creado)
-python manage.py shell <<EOF
-from apps.user.models import UserAccount
-from django.db.utils import IntegrityError
-
-try:
-    # Verificar si el superusuario ya existe
-    if not UserAccount.objects.filter(email="$EMAIL").exists():
-        # Crear superusuario
-        UserAccount.objects.create_superuser(email="$EMAIL", password="$PASSWORD", first_name="Admin", last_name="User")
-        print("Superuser created successfully.")
-    else:
-        print("Superuser already exists.")
-except IntegrityError as e:
-    print(f"Error creating superuser: {e}")
-EOF
